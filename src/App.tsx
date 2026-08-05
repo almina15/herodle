d08883cd185a9722c9de2f1e09138bfdb1856290
almina15 <dizdarevicalmina3@gmail.com>
2026-08-06 00:59:14 +0200

    glupi datum i dani

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function getDailyIndex(): number {
 
 function getDailyNumber(): number {
   const n = new Date()
-  const origin = Date.UTC(2026, 7, 6)
+  const origin = Date.UTC(2026, 8, 6)
   const ms = Date.UTC(n.getUTCFullYear(), n.getUTCMonth(), n.getUTCDate())
   return Math.floor((ms - origin) / 86_400_000) + 1
 }
